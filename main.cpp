91ce9deeade8179847853b84179486d4b6c74818
Angus Dippenaar <angusdippenaar@gmail.com>
2017-08-01 15:44:46 +0200

    Separated ostream into different files

--- main.cpp
+++ main.cpp
@@ -9,43 +9,8 @@
 static constexpr unsigned HEIGHT = 720;
 static constexpr unsigned WIDTH = 1024;
 
-bool processInput(InputManager &inputManager);
+bool	processInput(InputManager &inputManager);
 void	openGLError();
-
-std::ostream &operator<<(std::ostream &out, const glm::vec4 &rhs) {
-	out << "[ " << rhs.x << ", " << rhs.y << ", " << rhs.z << ", " << rhs.w << " ]";
-
-	return out;
-}
-
-std::ostream &operator<<(std::ostream &out, const glm::vec3 &rhs) {
-	out << "{ " << rhs.x << ", " << rhs.y << ", " << rhs.z << " }";
-
-	return out;
-}
-
-std::ostream &operator<<(std::ostream &out, const glm::vec2 &rhs) {
-	out << "{ " << rhs.x << ", " << rhs.y << " }";
-
-	return out;
-}
-
-std::ostream &operator<<(std::ostream &out, const Vertex &rhs) {
-	out << "{ { " << rhs.position.x << ", " << rhs.position.y << ", " << rhs.position.z << " }, ";
-	out << "{ " << rhs.normal.x << ", " << rhs.normal.y << ", " << rhs.normal.z << " }, ";
-	out << "{ " << rhs.uv.x << ", " << rhs.uv.y << " } }";
-
-	return out;
-}
-
-std::ostream &operator<<(std::ostream &out, const glm::mat4 &rhs) {
-	out << "[ " << rhs[0][0] << ", " << rhs[0][1] << ", " << rhs[0][2] << ", " << rhs[0][3] << " ]\n";
-	out << "[ " << rhs[1][0] << ", " << rhs[1][1] << ", " << rhs[1][2] << ", " << rhs[1][3] << " ]\n";
-	out << "[ " << rhs[2][0] << ", " << rhs[2][1] << ", " << rhs[2][2] << ", " << rhs[2][3] << " ]\n";
-	out << "[ " << rhs[3][0] << ", " << rhs[3][1] << ", " << rhs[3][2] << ", " << rhs[3][3] << " ]\n";
-
-	return out;
-}
 
 int main() {
 	Window			window("Bomberman", WIDTH, HEIGHT, Window::Flags::VSYNC_ENABLED);
@@ -117,13 +82,8 @@ int main() {
 			19, 17, 18
 	};
 
-//	for (auto &vertex : vertices) {
-//		std::cout << vertex << "\n";
-//	}
-
 	Model	model = loader.loadToVAO(vertices, indices, "../image.png");
 	Model	model2 = loader.loadToVAO(vertices, indices2, "../image2.png");
-//	Model	model = loader.loadToVAO(vertices, normals, uv, indices, "../image.png");
 	Entity	entity(glm::vec3(0.0f, 0.0f, -5.0f), glm::vec3(0.0f, 0.0f, -5.0f), 1.0f, model);
 
 	std::vector<Entity>		entities {
@@ -155,10 +115,6 @@ int main() {
 		shader.start();
 
 		camera.update(inputManager, shader);
-//		glm::mat4 viewMatrix = Maths::createViewMatrix(camera);
-//		shader.uploadMatrix4f(shader.getUniformLocation("viewMatrix"), viewMatrix);
-
-//		std::cout << "View Matrix: " << viewMatrix << "\n";
 		renderer.render(entity);
 
 		float rotation = 0.05f;
@@ -200,34 +156,3 @@ bool processInput(InputManager &inputManager) {
 
 }
 
-void	openGLError() {
-	GLenum error = glGetError();
-
-	switch (error) {
-		case GL_NO_ERROR:
-			break;
-		case GL_INVALID_ENUM:
-			std::cout << "Error: " << error << " Invalid enum\n";
-			break;
-		case GL_INVALID_VALUE:
-			std::cout << "Error: " << error << " Invalid value\n";
-			break;
-		case GL_INVALID_OPERATION:
-			std::cout << "Error: " << error << " Invalid operation\n";
-			break;
-		case GL_INVALID_FRAMEBUFFER_OPERATION:
-			std::cout << "Error: " << error << "Invalid frambuffer operation\n";
-			break;
-		case GL_OUT_OF_MEMORY:
-			std::cout << "Error: " << error << " Out of memory\n";
-			break;
-		case GL_STACK_OVERFLOW:
-			std::cout << "Error: " << error << " Stack underflow\n";
-			break;
-		case GL_STACK_UNDERFLOW:
-			std::cout << "Error: " << error << " Stack underflow\n";
-			break;
-		default:
-			std::cout << "Error: " << error << "\n";
-	}
-}
