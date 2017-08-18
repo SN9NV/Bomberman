be62e764a363d5a61082e3cd666756bdec4f040e
Robert Jones <rojones@e5r10p5.wtc.co.za>
2017-08-18 14:28:25 +0200

    randermisation of ballon movement

--- BomberManGameLogic/Balloon.cpp
+++ BomberManGameLogic/Balloon.cpp
@@ -4,7 +4,7 @@
 
 #include "Balloon.hpp"
 
-std::uniform_int_distribution<int> _ndist(0,1);
+std::uniform_int_distribution<int> _ndisision(0,1);
 std::default_random_engine gen;
 
 Balloon::Balloon(const glm::vec3 &position, const glm::vec3 &rotation, float scale, cge::Model &model)
@@ -21,18 +21,15 @@ Balloon::Balloon(const glm::vec3 &position, const glm::vec3 &rotation, float sca
 
 bool Balloon::update(const cge::InputManager &input, cge::GLSLProgram &shader, unsigned lastFrameTime)
 {
-	srand((unsigned int) time(NULL));
-	if (_n_moveDir.x == 0 && _n_moveDir.z == 0)
+	static int changeDir = 0;
+	if ((_n_moveDir.x == 0 && _n_moveDir.z == 0) || (changeDir == 1000))
 	{
-		/*if (rand() % 2 == 1)
-			_n_moveDir.x = (rand() % 3) - 1;
+		if (_ndisision(gen) == 1)
+			_n_moveDir.x = (_ndisision(gen) == 1) ? -1 : 1;
 		else
-			_n_moveDir.z = (rand() % 3) - 1;*/
-		if (_ndist(gen) == 1)
-			_n_moveDir.x = (_ndist(gen) == 1) ? -1 : 1;
-		else
-			_n_moveDir.z = (_ndist(gen) == 1) ? -1 : 1;
+			_n_moveDir.z = (_ndisision(gen) == 1) ? -1 : 1;
 	}
+	changeDir = (changeDir == 1000) ? 0 : changeDir + 1;
 	if (lastFrameTime < 500)
 		return (Being::update(input, shader, lastFrameTime));
 	return (true);
