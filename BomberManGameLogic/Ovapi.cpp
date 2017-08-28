25a565856c59cafaf74c298d8328cc05011a3860
Robert JONES <rojones@e5r10p5.wethinkcode.co.za>
2017-08-28 07:06:31 +0200

    ovapi tidie up

--- BomberManGameLogic/Ovapi.cpp
+++ BomberManGameLogic/Ovapi.cpp
@@ -2,6 +2,7 @@
 // Created by rojones on 2017/08/25.
 //
 
+#include <random>
 #include "Ovapi.hpp"
 
 static std::uniform_int_distribution<int> _n_disision(0, 1);
@@ -13,72 +14,55 @@ Ovapi::Ovapi(const glm::vec3 &position, const glm::vec3 &rotation, float scale, 
 		Being(position, rotation, scale, model, loader, hitBox, 0.002f, 0, true),
 		_changeDir(0),
 		_player(player),
-		_level(level)
-{
+		_level(level) {
 
 }
 
-bool Ovapi::update(const cge::InputManager &input, cge::GLSLProgram &shader, unsigned lastFrameTime)
-{
+bool Ovapi::update(const cge::InputManager &input, cge::GLSLProgram &shader, unsigned lastFrameTime) {
 	int moveDir;
 	float pX, pZ;
-	pX = static_cast<float>(round(_player.getPosition().x));
-	pZ = static_cast<float>(round(_player.getPosition().z));
+	pX = roundf(_player.getPosition().x);
+	pZ = roundf(_player.getPosition().z);
 	float x;
 	float z;
 	static const int timeToRandom = 1000;
-	/*if (_n_moveDir.x == 0 && _n_moveDir.z == 0) {
-		if (_n_disision(_gen) == 1) {
-			moveDir = (_n_disision(_gen) == 1) ? -1 : 1;
-			_n_moveDir.x = ((int) _n_moveDir.x == moveDir) ? -moveDir : moveDir;
-		} else {
-			moveDir = (_n_disision(_gen) == 1) ? -1 : 1;
-			_n_moveDir.z = ((int) _n_moveDir.z == moveDir) ? -moveDir : moveDir;
-		}
-	}
-	else*/ if ((fmod(_position.x, 1) > 1 - _speed ||
-				fmod(_position.x, 1) < _speed && pZ == roundf(this->getPosition().z) &&
-				fabs(_player.getPosition().x - _position.x) < 6) && (_n_moveDir != glm::vec3({0, 0, 0})))
-	{
+	if (((fmod(_position.x, 1) > 1 - _speed || fmod(_position.x, 1) < _speed)
+		&& floor(_player.getPosition().z) == floor(this->getPosition().z)
+		&& fabs(_player.getPosition().x - _position.x) < 6)
+		&& (_n_moveDir != glm::vec3({0, 0, 0}))) {
 		_n_moveDir.z = 0;
 		_position.x = roundf(_position.x);
 		if (pX > _position.x)
 			_n_moveDir.x = 1;
 		else if (pX < _position.x)
 			_n_moveDir.x = -1;
-	} else if ((fmod(_position.z, 1) > 1 - _speed ||
-				fmod(_position.z, 1) < _speed && pX == roundf(this->getPosition().x) &&
-				fabs(_player.getPosition().x - _position.x) < 6) && (_n_moveDir != glm::vec3({0, 0, 0})))
-	{
+	} else if (((fmod(_position.z, 1) > 1 - _speed || fmod(_position.z, 1) < _speed)
+				&& floor(_player.getPosition().x) == floor(this->getPosition().x)
+				&& fabs(_player.getPosition().x - _position.x) < 6)
+				&& (_n_moveDir != glm::vec3({0, 0, 0}))) {
 		_n_moveDir.x = 0;
 		_position.z = roundf(_position.z);
 		if (pZ > _position.z)
 			_n_moveDir.z = 1;
 		else if (pZ < _position.z)
 			_n_moveDir.z = -1;
-	} else if (_changeDir >= timeToRandom && _n_change(_gen) == 1)
-	{
+	} else if (_changeDir >= timeToRandom && _n_change(_gen) == 1) {
 		x = fmodf(_position.x, 1);
 		z = fmodf(_position.z, 1);
-		if ((z < 0.01 || z > 0.99))
-		{
+		if ((z < 0.01 || z > 0.99)) {
 			_n_moveDir.x = 0;
 			_n_moveDir.z = (_n_disision(_gen) == 1) ? -1 : 1;
 			_position.z = static_cast<float>(round(_position.z));
-		} else if ((x < 0.01 || x > 0.99))
-		{
+		} else if ((x < 0.01 || x > 0.99)) {
 			_n_moveDir.z = 0;
 			_n_moveDir.x = (_n_disision(_gen) == 1) ? -1 : 1;
 			_position.x = static_cast<float>(round(_position.x));
 		}
-	} else if (_n_moveDir.x == 0 && _n_moveDir.z == 0)
-	{
-		if (_n_disision(_gen) == 1)
-		{
+	} else if (_n_moveDir.x == 0 && _n_moveDir.z == 0) {
+		if (_n_disision(_gen) == 1) {
 			moveDir = (_n_disision(_gen) == 1) ? -1 : 1;
 			_n_moveDir.x = ((int) _n_moveDir.x == moveDir) ? -moveDir : moveDir;
-		} else
-		{
+		} else {
 			moveDir = (_n_disision(_gen) == 1) ? -1 : 1;
 			_n_moveDir.z = ((int) _n_moveDir.z == moveDir) ? -moveDir : moveDir;
 		}
